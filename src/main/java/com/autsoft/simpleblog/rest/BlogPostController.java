5028abcef2d0d7bbf7be30f6936ec448cd8fe561
Kovacs Bence <bence.kovacs.zoltan@gmail.com>
2021-05-19 11:10:55 +0200

    create CategoryController and define api

--- src/main/java/com/autsoft/simpleblog/rest/BlogPostController.java
+++ src/main/java/com/autsoft/simpleblog/rest/BlogPostController.java
@@ -1,6 +1,6 @@
 package com.autsoft.simpleblog.rest;
+
 import com.autsoft.simpleblog.dto.BlogPostDTO;
-import com.autsoft.simpleblog.dto.CategoryDTO;
 import com.autsoft.simpleblog.model.BlogPost;
 import com.autsoft.simpleblog.service.BlogPostService;
 import org.springframework.data.domain.Page;
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 @RestController("/api/blogPosts")
@@ -16,40 +15,46 @@ public class BlogPostController {
 
     private BlogPostService blogPostService;
 
-    BlogPostController(final BlogPostService blogPostService){
+    BlogPostController(final BlogPostService blogPostService) {
         this.blogPostService = blogPostService;
     }
 
+
     // functionality
     @GetMapping("/search/{tag}")
-    public ResponseEntity<Page<BlogPost>> searchByTags(@PathVariable String tag){
+    public ResponseEntity<Page<BlogPost>> searchByTags(@PathVariable final String tag) {
         return null;
     }
 
     @PostMapping("/{id}/addCategory/{categoryName}")
-    public ResponseEntity<BlogPost> addCategoryToBlogPost(@PathVariable Long id, @PathVariable String categoryName){
+    public ResponseEntity<BlogPost> addCategoryToBlogPost(@PathVariable final Long id, @PathVariable final String categoryName) {
         return null;
     }
 
     @DeleteMapping("/{id}/removeCategory/{categoryName}")
-    public ResponseEntity<BlogPost> removeCategoryFromBlogPost(@PathVariable Long id, @PathVariable String categoryName){
+    public ResponseEntity<BlogPost> removeCategoryFromBlogPost(@PathVariable final Long id, @PathVariable final String categoryName) {
         return null;
     }
 
     // CRUD
+    @GetMapping("/{id}")
+    public ResponseEntity<BlogPost> getBlogPost(@PathVariable final Long id) {
+        return null;
+    }
+
     @PostMapping
-    public ResponseEntity<BlogPost> addBlogPost(@RequestBody @Valid BlogPostDTO blogPostDTO){
+    public ResponseEntity<BlogPost> createBlogPost(@RequestBody @Valid final BlogPostDTO blogPostDTO) {
         return null;
     }
 
     // doesn't update blogpost's categories, so this could be a patch as well. I decided to use PUT
     @PutMapping("/{id}")
-    public ResponseEntity<BlogPost> updateBlogPost(@PathVariable Long id, @RequestBody @Valid BlogPostDTO blogPostDTO) throws URISyntaxException {
+    public ResponseEntity<BlogPost> updateBlogPost(@PathVariable final Long id, @RequestBody @Valid final BlogPostDTO blogPostDTO) throws URISyntaxException {
         return null;
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBlogPost(@PathVariable Long id){
+    public ResponseEntity<Void> deleteBlogPost(@PathVariable final Long id) {
         return null;
     }
 
--- src/main/java/com/autsoft/simpleblog/rest/CategoryController.java
+++ src/main/java/com/autsoft/simpleblog/rest/CategoryController.java
@@ -1,4 +1,44 @@
 package com.autsoft.simpleblog.rest;
 
+import com.autsoft.simpleblog.dto.CategoryDTO;
+import com.autsoft.simpleblog.model.Category;
+import com.autsoft.simpleblog.service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URISyntaxException;
+
+
+@RestController("/api/categories")
 public class CategoryController {
+
+
+    private CategoryService categoryService;
+
+    CategoryController(final CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // CRUD
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategory(@PathVariable final Long id) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody @Valid final CategoryDTO categoryDTO) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable final Long id, @RequestBody @Valid final CategoryDTO categoryDTO) throws URISyntaxException {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable final Long id) {
+        return null;
+    }
+
 }
