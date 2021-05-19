9b99506c4cf6e152e36f9408cc6c7ffbdad88b08
Kovacs Bence <bence.kovacs.zoltan@gmail.com>
2021-05-19 21:46:19 +0200

    hotfix - remove a test value from query

--- src/main/java/com/autsoft/simpleblog/repository/BlogPostRepository.java
+++ src/main/java/com/autsoft/simpleblog/repository/BlogPostRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.math.BigInteger;
-import java.util.List;
 
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
 
@@ -16,7 +14,7 @@ public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
     // I query all the ids for blog posts that have categories that have tags labelled the given label
     // this is probably not the best way to implement but I didn't find any built-in help for such a query
     // I'm out of time to index properly -> if i have time I'll define indexes so this query is not that slow.
-    @Query(value = "SELECT DISTINCT bp.id FROM category c JOIN category_tags ct ON c.id = ct.category_id JOIN tag ON tag.id = ct.tag_id JOIN blog_post_categories bpc ON bpc.category_id = ct.category_id JOIN blog_post bp ON bp.id = bpc.blog_post_id WHERE tag.label=\"abc\"",
+    @Query(value = "SELECT DISTINCT bp.id FROM category c JOIN category_tags ct ON c.id = ct.category_id JOIN tag ON tag.id = ct.tag_id JOIN blog_post_categories bpc ON bpc.category_id = ct.category_id JOIN blog_post bp ON bp.id = bpc.blog_post_id WHERE tag.label=?1",
     nativeQuery = true)
     Page<BigInteger> findBlogPostsWithCategoriesTaggedPaged(final String label, Pageable pageable);
 
